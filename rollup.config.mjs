ad46460c21c9427efa5e6ab1e50e38a81a4e4483
Pratik Kajare <developerpratikkajare@gmail.com>
2023-02-23 11:42:19 +0530

    releasing npm version 0.0.2

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commmonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
-
+import postcss from 'rollup-plugin-postcss';
 import packageJson from './package.json' assert { type: 'json' };
 
 export default [
@@ -20,11 +20,12 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commmonjs(), typescript({ tsconfig: './tsconfig.json' })],
+    plugins: [resolve(), commmonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss()],
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
